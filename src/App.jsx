061358518d6cb0ef7ec142e885eb2808822ddfe2
hunterhhh412 <amanagnihotri412002@gmail.com>
2024-04-14 22:07:11 +0530

    The Login and Sign-Up page is functional now, along with a chat list of the users.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import Login from "./components/login/Login";
 import { useEffect } from "react";
+import Login from "./components/login/Login";
+import List from "./components/list/List";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
-    <div className=''>
-      <Login/>
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App
